7ca965a0f701f71b379a13b1c031294d85408d10
joaopedrocoder <joaopedrolr.7@gmail.com>
2023-12-12 23:32:00 -0300

    desenvolvida a logica de de edição e novo

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -5,31 +5,33 @@ import { Category } from '../models/category.model';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrl: './category-list.component.scss'
+  styleUrl: './category-list.component.scss',
 })
 export class CategoryListComponent {
-  categories: Category[] = []
-  constructor(private categoryService: CategoryService) {} 
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.listCategories()
+    this.listCategories();
   }
 
   listCategories() {
     this.categoryService.getAll().subscribe({
-      next: response => this.categories = response,
-      error: err => console.log(err) 
-    })
+      next: (response) => (this.categories = response),
+      error: (err) => console.log(err),
+    });
   }
 
   deleteCategory(id: number): void {
-    const mustDelete = confirm('Deseja realmente excluir este item?')
+    const mustDelete = confirm('Deseja realmente excluir este item?');
 
-    if(mustDelete)
-    this.categoryService.delete(id).
-      subscribe({
-        next: response => this.categories = this.categories.filter(category => category.id !== id),
-        error: err => console.log(err)
-      })
+    if (mustDelete)
+      this.categoryService.delete(id).subscribe({
+        next: (response) =>
+          (this.categories = this.categories.filter(
+            (category) => category.id !== id
+          )),
+        error: (err) => console.log(err),
+      });
   }
 }
--- src/app/pages/categories/services/category.service.ts
+++ src/app/pages/categories/services/category.service.ts
@@ -4,46 +4,46 @@ import { Observable, catchError, map, throwError } from 'rxjs';
 import { Category } from '../models/category.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
-  private baseUrl = 'api/categories'
+  private baseUrl = 'api/categories';
 
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
+
   getAll(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.baseUrl)
+    return this.http.get<Category[]>(this.baseUrl);
   }
 
-  getById(id: number): Observable<Category>{
-    return this.http.get<Category>(`${this.baseUrl}/${id}`).pipe(
-      catchError(this.handleError)
-    )
+  getById(id: number): Observable<Category> {
+    return this.http
+      .get<Category>(`${this.baseUrl}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   create(category: Category): Observable<Category> {
-    return this.http.post<Category>(this.baseUrl, category).pipe(
-      catchError(this.handleError)
-    )
+    return this.http
+      .post<Category>(this.baseUrl, category)
+      .pipe(catchError(this.handleError));
   }
 
   update(category: Category, id: number): Observable<Category> {
     return this.http.post(`${this.baseUrl}/${id}`, category).pipe(
       catchError(this.handleError),
       map(() => category)
-    )
+    );
   }
 
   delete(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/${id}`).pipe(
-      catchError(this.handleError)
-    )
+    return this.http
+      .delete<any>(`${this.baseUrl}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   // private jsonDataCategories(jsonData: any[]): Category[] {
   //   const categories: Category[] = []
   //   jsonData.forEach(element => categories.push(element as Category))
-    
+
   //   return categories
   // }
 
@@ -52,8 +52,8 @@ export class CategoryService {
   // }
 
   private handleError(error: any): Observable<any> {
-    console.log('Erro na requisição: ', error)
-    const err = new Error('test') 
-    return throwError(() => err)
+    console.log('Erro na requisição: ', error);
+    const err = new Error('test');
+    return throwError(() => err);
   }
 }
