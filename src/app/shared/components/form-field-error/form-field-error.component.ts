d0e2bf981fdff5ffbbb3515f00c35f4c7ec76c45
joaopedrocoder <joaopedrolr.7@gmail.com>
2023-12-27 11:18:59 -0300

    criação do componente page header

---
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field-error',
+  templateUrl: './form-field-error.component.html',
+  styleUrl: './form-field-error.component.scss'
+})
+export class FormFieldErrorComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormFieldErrorComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
